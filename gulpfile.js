d9cee589039fc45d854d036c21e9396ec6a83e82
Joel McCance <jmccance@gmail.com>
2015-08-22 11:44:57 -0500

    Added linting for JavaScript templates.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 var src = {
-  generators: ['app/index.js']
+  generators: ['app/index.js'],
+  jsTemplates: ['app/templates/js/**/*.js']
 }
 
-gulp.task('lint', function () {
+gulp.task('lint-generators', function () {
   return gulp.src(src.generators)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
+
+gulp.task('lint-js-templates', function () {
+  return gulp.src(src.jsTemplates)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint', ['lint-generators', 'lint-js-templates']);
